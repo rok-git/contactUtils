c9bc277cd5b1280e73f8d89cace20cc0add4818e
C K Rok <kyong6+git@gmail.com>
2022-05-19 09:52:46 +0900

    '-a' option to include all contacts..

--- cn2vcf.m
+++ cn2vcf.m
@@ -10,6 +10,7 @@ void usage(char * me)
 {
     printf("Usage: %s -I id > data.vcf\n", me);
     printf("       %s name > data.vcf\n", me);
+    printf("       %s -a > data.vcf\n", me);
     exit(1);
 }
 
@@ -18,11 +19,14 @@ int main(int argc, char *argv[])
 {
     @autoreleasepool{
         NSString *name;
-        BOOL idSearch = NO;
         char *me = argv[0];
+        BOOL idSearch = NO, showAll = NO;
         char sw;
-        while((sw = getopt(argc, argv, "I")) != -1){
+        while((sw = getopt(argc, argv, "aI")) != -1){
             switch(sw){
+                case 'a':
+                    showAll = YES;
+                    break;
                 case 'I':
                     // search identifier
                     idSearch = YES;
@@ -31,10 +35,17 @@ int main(int argc, char *argv[])
         }
         argc -= optind;
         argv += optind;
-        if(argc != 0)
-            name = [NSString stringWithUTF8String: argv[0]];
-        else
-            usage(basename(me));
+        if(!showAll){
+            if(argc != 0)
+                name = [NSString stringWithUTF8String: argv[0]];
+            else
+                usage(basename(me));
+        }else{  // all contacts
+            if((argc != 0) || (idSearch)){
+                printf("'-a' cannot be used with other options nor arguments.\n");
+                return 1;
+            }
+        }
 
         if([CNContactStore authorizationStatusForEntityType: CNEntityTypeContacts] != CNAuthorizationStatusAuthorized){
             NSLog(@"%@", @"no authorization");
@@ -48,24 +59,37 @@ int main(int argc, char *argv[])
         dup2(fd, STDERR_FILENO);
 #endif
 
-        CNContactStore *store = [[CNContactStore alloc] init];
-        NSPredicate *predicate;
-        if(idSearch)
-            predicate = [CNContact predicateForContactsWithIdentifiers: @[name.stringByRemovingPercentEncoding]];
-        else
-            predicate = [CNContact predicateForContactsMatchingName: name];
         NSError *err;
-        NSMutableArray *keys = [@[CNContactPhoneNumbersKey, CNContactGivenNameKey, CNContactFamilyNameKey, CNContactOrganizationNameKey] mutableCopy];
-        [keys addObject: [CNContactVCardSerialization descriptorForRequiredKeys]];
-        NSArray *contacts = [store unifiedContactsMatchingPredicate: predicate keysToFetch: keys error: &err];
+        NSArray *contacts;
+        NSArray *keys = @[[CNContactVCardSerialization descriptorForRequiredKeys]];
+        CNContactStore *store = [[CNContactStore alloc] init];
+        if(!showAll){
+            NSPredicate *predicate;
+            if(idSearch)
+                predicate = [CNContact predicateForContactsWithIdentifiers: @[name.stringByRemovingPercentEncoding]];
+            else
+                predicate = [CNContact predicateForContactsMatchingName: name];
+            contacts = [store unifiedContactsMatchingPredicate: predicate keysToFetch: keys error: &err];
+        }else{ // show all
+            CNContactFetchRequest *request = [[CNContactFetchRequest alloc] initWithKeysToFetch: keys];
+            __block NSMutableArray *allContacts = [@[] mutableCopy];
+            BOOL success = [store enumerateContactsWithFetchRequest: request
+                error: &err
+                usingBlock: ^(CNContact * _Nonnull contact, BOOL * _Nonnull stop){
+                    [allContacts addObject: contact];
+                }
+            ];
+            if(err)
+                return 1;
+            contacts = [allContacts copy];
+        }
+        if(err)
+            return 1;
 
 #ifdef DONT_SHOW_STDERR
         dup2(fdx, STDERR_FILENO);
         close(fd);
 #endif 
-
-        if(err)
-            return 1;
 
         NSFileHandle *stdOut = [NSFileHandle fileHandleWithStandardOutput];
         NSData *vcfData = [CNContactVCardSerialization dataWithContacts: contacts error: &err];
